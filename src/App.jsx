c7256eb3de00dd4fa906a588dcb9f7cefa0680e6
sihyung <buzzcatfish@gmail.com>
2025-01-14 00:10:34 +0900

    ``` feat: TailwindCSS 및 관련 의존성 추가
    - TailwindCSS 환경 설정을 위해 tailwind.config.js 파일 생성
    - TailwindCSS, autoprefixer, postcss 관련 패키지 추가
    - 'Plus Jakarta Sans' 폰트를 사용하는 TailwindCSS 테마 확장
    - 프로젝트에 lucide-react 추가
    ```

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,61 @@
 import { useEffect, useState } from "react";
+import { Sun, Wind, Sunrise, Thermometer } from "lucide-react";
 
 function App() {
   const API_KEY = "1c6040609dd62a847ede395d2b820d43";
-  const city = "Edinburgh";
-  const [data, setData] = useState(null);
+  const [coord, setCoord] = useState({ lat: 37.5665, lon: 126.978 });
+  const [weatherData, setWeatherData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     (async function () {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
-      );
-      const result = await response.json();
-      setData(result);
-      setIsLoading(false);
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/3.0/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${API_KEY}&units=metric`,
+        );
+        const result = await response.json();
+        setWeatherData(result);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
     })();
   }, []);
-  if (!isLoading) {
-    console.log(data);
-  }
+
   return (
     <>
-      <div className="location">
-        {data.name}, {data.sys.country}
-      </div>
-      <div className="current-weather">
-        <span className="weather-icon">
-          <img
-            src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-            alt={data.weather[0].description}
-          />
-        </span>
-        <span className="temperature">19°</span>
-
-        <span className="description">data.weather[0].main</span>
-      </div>
-      <div className="weekly-forecast">
-        <ul>
-          <li className="forecast-item">
-            <span className="forecast-date">Wed Jun 21</span>
-            <span className="forecast-temp">29° 13°C</span>
-          </li>
-          <li className="forecast-item">
-            <span className="forecast-date">Thu Jun 22</span>
-            <span className="forecast-temp">26° 10°C</span>
-          </li>
-          <li className="forecast-item">
-            <span className="forecast-date">Fri Jun 23</span>
-            <span className="forecast-temp">26° 12°C</span>
-          </li>
-        </ul>
-      </div>
+      {isLoading ? (
+        <div className="flex items-center justify-center min-h-screen ">
+          <div className="text-xl text-gray-600">Loading...</div>
+        </div>
+      ) : (
+        <>
+          <div className="min-h-screen ">
+            <header className=" p-6 md:p-8">
+              <div className="text-center">{weatherData.timezone}</div>
+              <div className="flex flex-col items-center justify-between mt-8">
+                <div className="flex items-center gap-4">
+                  {weatherData.current.weather[0].icon && (
+                    <img
+                      src={`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`}
+                      alt={weatherData.current.weather[0].description}
+                      className="w-32 h-32 md:w-24 md:h-24"
+                    />
+                  )}
+                </div>
+                <div className="text-7xl md:text-8xl font-bold mb-8">
+                  {Math.round(weatherData.current.temp)}°
+                </div>
+                <p className="text-xl md:text-2xl capitalize">
+                  {weatherData.current.weather[0].description}
+                </p>
+              </div>
+            </header>
+          </div>
+        </>
+      )}
     </>
   );
 }
