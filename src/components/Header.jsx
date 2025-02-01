734e7bdb309dd3fc491ee0730236747ec255ab9f
sihyung <l_sh@athomecorp.com>
2025-02-01 22:41:18 +0900

    feat: Header 컴포넌트에 검색 기능 추가
    - 기존 Ellipsis 버튼을 제거하고 검색 입력 필드와 버튼 추가
    - 사용자가 장소를 검색할 수 있도록 UI 개선

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
-import { Ellipsis } from "lucide-react";
+import { Search } from "lucide-react";
 
 export const Header = () => {
   return (
     <div className="flex justify-between items-center">
       <h1 className="text-2xl font-bold">forecazt</h1>
-      <button className="text-gray-600">
-        <Ellipsis size={25} />
-      </button>
+      <div className="flex justify-end">
+        <input
+          type="text"
+          placeholder="Search for places"
+          className="w-full mr-2 border border-gray-300 rounded-md p-2 text-xs align-middle"
+        />
+        <button className="text-gray-600 align-middle">
+          <Search size={25} />
+        </button>
+      </div>
     </div>
   );
 };
